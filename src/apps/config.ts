b218cf487828273d033f4bdb0a5bc6f146c31e87
CodFrm <i@xloli.top>
2022-01-17 17:14:37 +0800

    💚 修复ci打包版本号错误

--- src/apps/config.ts
+++ src/apps/config.ts
@@ -1,4 +1,4 @@
-export const ExtVersion = '0.8.0';
+export const ExtVersion = "0.8.1";
 
 export const Server = process.env.NODE_ENV == 'production' ? 'https://sc.icodef.com/' : 'http://localhost:8080/';
 
