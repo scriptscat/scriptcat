725e5bada755ec8bf74a804d8340a81bd8dcd12f
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-07-06 19:14:58 +0900

    test01

--- src/app/service/content/inject.ts
+++ src/app/service/content/inject.ts
@@ -3,8 +3,6 @@ import { ExecScript, ValueUpdateData } from "./exec_script";
 import { addStyle, ScriptFunc } from "./utils";
 import { getStorageName } from "@App/pkg/utils/utils2";
 import { EmitEventRequest, ScriptLoadInfo } from "../service_worker/runtime";
-import { ExternalWhitelist } from "@App/app/const";
-import { sendMessage } from "@Packages/message/client";
 
 export class InjectRuntime {
   execList: ExecScript[] = [];
@@ -15,112 +13,14 @@ export class InjectRuntime {
     private scripts: ScriptLoadInfo[]
   ) {}
 
-  start() {
-    this.scripts.forEach((script) => {
-      // @ts-ignore
-      const scriptFunc = window[script.flag];
-      if (scriptFunc) {
-        this.execScript(script, scriptFunc);
-      } else {
-        // 监听脚本加载,和屏蔽读取
-        Object.defineProperty(window, script.flag, {
-          configurable: true,
-          set: (val: ScriptFunc) => {
-            this.execScript(script, val);
-          },
-        });
-      }
-    });
-    this.server.on("runtime/emitEvent", (data: EmitEventRequest) => {
-      // 转发给脚本
-      const exec = this.execList.find((val) => val.scriptRes.uuid === data.uuid);
-      if (exec) {
-        exec.emitEvent(data.event, data.eventId, data.data);
-      }
-    });
-    this.server.on("runtime/valueUpdate", (data: ValueUpdateData) => {
-      this.execList
-        .filter((val) => val.scriptRes.uuid === data.uuid || getStorageName(val.scriptRes) === data.storageName)
-        .forEach((val) => {
-          val.valueUpdate(data);
-        });
-    });
-    // 注入允许外部调用
-    this.externalMessage();
-  }
+  start() {}
 
-  externalMessage() {
-    // 对外接口白名单
-    let msg = this.msg;
-    for (let i = 0; i < ExternalWhitelist.length; i += 1) {
-      if (window.location.host.endsWith(ExternalWhitelist[i])) {
-        // 注入
-        (<{ external: any }>(<unknown>window)).external = window.external || {};
-        (<
-          {
-            external: {
-              Scriptcat: {
-                isInstalled: (name: string, namespace: string, callback: any) => void;
-              };
-            };
-          }
-        >(<unknown>window)).external.Scriptcat = {
-          async isInstalled(name: string, namespace: string, callback: any) {
-            const resp = await sendMessage(msg, "content/script/isInstalled", {
-              name,
-              namespace,
-            });
-            callback(resp);
-          },
-        };
-        (<{ external: { Tampermonkey: any } }>(<unknown>window)).external.Tampermonkey = (<
-          { external: { Scriptcat: any } }
-        >(<unknown>window)).external.Scriptcat;
-        break;
-      }
-    }
-  }
+  externalMessage() {}
 
-  execScript(script: ScriptLoadInfo, scriptFunc: ScriptFunc) {
-    // @ts-ignore
-    delete window[script.flag];
-    const exec = new ExecScript(script, "content", this.msg, scriptFunc);
-    this.execList.push(exec);
-    // 注入css
-    if (script.metadata["require-css"]) {
-      script.metadata["require-css"].forEach((val) => {
-        const res = script.resource[val];
-        if (res) {
-          addStyle(res.content);
-        }
-      });
-    }
-    if (script.metadata["run-at"] && script.metadata["run-at"][0] === "document-body") {
-      // 等待页面加载完成
-      this.waitBody(() => {
-        exec.exec();
-      });
-    } else {
-      exec.exec();
-    }
-  }
+  execScript(script: ScriptLoadInfo, scriptFunc: ScriptFunc) {}
 
   // 参考了tm的实现
-  waitBody(callback: () => void) {
-    if (document.body) {
-      callback();
-      return;
-    }
-    const listen = () => {
-      document.removeEventListener("load", listen, false);
-      document.removeEventListener("DOMNodeInserted", listen, false);
-      document.removeEventListener("DOMContentLoaded", listen, false);
-      this.waitBody(callback);
-    };
-    document.addEventListener("load", listen, false);
-    document.addEventListener("DOMNodeInserted", listen, false);
-    document.addEventListener("DOMContentLoaded", listen, false);
-  }
+  waitBody(callback: () => void) {}
 }
 
 export function generateRunTime(server: Server, msg: Message, scripts: ScriptLoadInfo[]){
