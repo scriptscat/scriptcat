18988b883ea8c99c1ed0bdd26450d09d69a28cd5
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-07-10 20:24:12 +0900

    单元测试修正

--- src/app/service/content/exec_script.test.ts
+++ src/app/service/content/exec_script.test.ts
@@ -53,7 +53,7 @@ describe("GM_info", () => {
     const ret = await noneExec.exec();
     expect(ret.GM_info.version).toEqual(ExtVersion);
     expect(ret.GM_info.script.version).toEqual("1.0.0");
-    expect(ret._this).toEqual({});
+    expect(ret._this).toEqual(global);
   });
   it("sandbox", async () => {
     scriptRes2.code = "return {_this:this,GM_info};";
