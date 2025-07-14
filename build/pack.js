824e275830047016129ff94755f6d1eed9e5ce3c
王一之 <yz@ggnb.top>
2025-07-14 22:36:06 +0800

    🐛 修复打包id问题

--- build/pack.js
+++ build/pack.js
@@ -67,11 +67,11 @@ delete firefoxManifest.sandbox;
 // "script-src 'self' blob:; object-src 'self' blob:";
 firefoxManifest.browser_specific_settings = {
   gecko: {
-    id: `${
+    id: `{${
       version.prerelease.length
-        ? "scriptcat-beta@scriptcat.org"
-        : "scriptcat@scriptcat.org"
-    }`,
+        ? "44ab8538-2642-46b0-8a57-3942dbc1a33b"
+        : "8e515334-52b5-4cc5-b4e8-675d50af677d"
+    }}`,
     strict_min_version: "91.1.0",
   },
 };
