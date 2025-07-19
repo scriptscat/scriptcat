4c677449eeec28be4334b203d04c6b95d36ff7f5
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-07-19 17:14:37 +0900

    Update ScriptList.tsx

--- src/pages/options/routes/ScriptList.tsx
+++ src/pages/options/routes/ScriptList.tsx
@@ -794,6 +794,7 @@ function ScriptList() {
                   <Typography.Text>{t("batch_operations")}:</Typography.Text>
                   <Select
                     style={{ minWidth: "100px" }}
+                    triggerProps={{ autoAlignPopupWidth: false, autoAlignPopupMinWidth: true, position: "bl" }}
                     size="mini"
                     value={action}
                     onChange={(value) => {
@@ -938,6 +939,7 @@ function ScriptList() {
                   <Typography.Text>{t("resize_column_width")}:</Typography.Text>
                   <Select
                     style={{ minWidth: "80px" }}
+                    triggerProps={{ autoAlignPopupWidth: false, autoAlignPopupMinWidth: true, position: "bl" }}
                     size="mini"
                     value={newColumns[selectColumn].title?.toString()}
                     onChange={(val) => {
