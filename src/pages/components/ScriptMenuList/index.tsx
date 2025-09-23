79bdb9ecd7cd9310d3c25e9b58d8c90be2d1b2a3
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-09-24 04:24:00 +0900

    handleEnableChange

--- src/pages/components/ScriptMenuList/index.tsx
+++ src/pages/components/ScriptMenuList/index.tsx
@@ -166,17 +166,6 @@ const ScriptMenuList = React.memo(
       };
     }, []);
 
-    const handleEnableChange = useCallback((item: ScriptMenu, checked: boolean) => {
-      scriptClient
-        .enable(item.uuid, checked)
-        .then(() => {
-          setList((prevList) => prevList.map((item1) => (item1 === item ? { ...item1, enable: checked } : item1)));
-        })
-        .catch((err) => {
-          Message.error(err);
-        });
-    }, []);
-
     const handleRunScript = useCallback((item: ScriptMenu) => {
       if (item.runStatus !== SCRIPT_RUN_STATUS_RUNNING) {
         runtimeClient.runScript(item.uuid);
@@ -209,50 +198,56 @@ const ScriptMenuList = React.memo(
       window.close();
     }, []);
 
-    const CollapseHeader = React.memo(
-      ({
-        item,
-        onEnableChange,
-      }: {
-        item: ScriptMenu;
-        onEnableChange: (item: ScriptMenu, checked: boolean) => void;
-      }) => {
-        return (
-          <div
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-            title={
-              item.enable
-                ? item.runNumByIframe
-                  ? t("script_total_runs", {
-                      runNum: item.runNum,
-                      runNumByIframe: item.runNumByIframe,
-                    })!
-                  : t("script_total_runs_single", { runNum: item.runNum })!
-                : t("script_disabled")!
-            }
-          >
-            <Space>
-              <Switch size="small" checked={item.enable} onChange={(checked) => onEnableChange(item, checked)} />
-              <span
-                style={{
-                  display: "block",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  whiteSpace: "nowrap",
-                  color: item.runNum === 0 ? "rgb(var(--gray-5))" : "",
-                  lineHeight: "20px",
-                }}
-              >
-                <ScriptIcons script={item} size={20} />
-                {i18nName(item)}
-              </span>
-            </Space>
-          </div>
-        );
-      }
-    );
+    const CollapseHeader = React.memo(({ item }: { item: ScriptMenu }) => {
+      const handleEnableChange = useCallback(
+        (checked: boolean) => {
+          scriptClient
+            .enable(item.uuid, checked)
+            .then(() => {
+              setList((prevList) => prevList.map((item1) => (item1 === item ? { ...item1, enable: checked } : item1)));
+            })
+            .catch((err) => {
+              Message.error(err);
+            });
+        },
+        [item.uuid]
+      );
+
+      return (
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          title={
+            item.enable
+              ? item.runNumByIframe
+                ? t("script_total_runs", {
+                    runNum: item.runNum,
+                    runNumByIframe: item.runNumByIframe,
+                  })!
+                : t("script_total_runs_single", { runNum: item.runNum })!
+              : t("script_disabled")!
+          }
+        >
+          <Space>
+            <Switch size="small" checked={item.enable} onChange={handleEnableChange} />
+            <span
+              style={{
+                display: "block",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                color: item.runNum === 0 ? "rgb(var(--gray-5))" : "",
+                lineHeight: "20px",
+              }}
+            >
+              <ScriptIcons script={item} size={20} />
+              {i18nName(item)}
+            </span>
+          </Space>
+        </div>
+      );
+    });
     CollapseHeader.displayName = "CollapseHeader";
 
     const ListMenuItem = React.memo(({ item, index }: { item: ScriptMenu; index: number }) => {
@@ -280,7 +275,7 @@ const ScriptMenuList = React.memo(
       return (
         <Collapse bordered={false} expandIconPosition="right" key={item.uuid}>
           <CollapseItem
-            header={<CollapseHeader item={item} onEnableChange={handleEnableChange} />}
+            header={<CollapseHeader item={item} />}
             name={item.uuid}
             contentStyle={{ padding: "0 0 0 40px" }}
           >
