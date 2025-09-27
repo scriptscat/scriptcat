62c107ba3a53d738cd71a6ba28ee76128f47b3d2
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-09-27 19:32:00 +0900

    useStableCallbacks 添加注解

--- src/pages/utils/utils.ts
+++ src/pages/utils/utils.ts
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 
+// 使用 useStableCallbacks 能降低开支，但要避免读取 state。
+// 因为不使用依赖检查, 不会检查依存state的更新来重新宣告 deps 物件里的function
 export function useStableCallbacks<T extends (...args: any[]) => any, D extends Record<string, T>>(deps: D) {
   const objectRef = useRef<D>();
   if (!objectRef.current) {
