1a4bdc0a482ce14af2b5a796912cd5360cbfe523
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-09-29 21:57:17 +0900

    🐛 typo fix (#776)
    * typo fix
    
    * Update main.tsx
    
    * fix

--- src/pages/components/layout/PopupLayout.tsx
+++ src/pages/components/layout/PopupLayout.tsx
@@ -2,10 +2,10 @@ import type { ReactNode } from "react";
 import React from "react";
 import "./index.css";
 
-const MainLayout: React.FC<{
+const PopupLayout: React.FC<{
   children: ReactNode;
 }> = ({ children }) => {
   return <div style={{ borderBottom: "1px solid var(--color-neutral-3)" }}>{children}</div>;
 };
 
-export default MainLayout;
+export default PopupLayout;
