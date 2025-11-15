ea13c13b4e9e3751b8e3f653eb9011cfda1518cc
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-11-15 18:47:33 +0900

    VSCodeConnect 代码优化

--- src/app/service/service_worker/client.ts
+++ src/app/service/service_worker/client.ts
@@ -19,7 +19,7 @@ import { v4 as uuidv4 } from "uuid";
 import { cacheInstance } from "@App/app/cache";
 import { CACHE_KEY_IMPORT_FILE } from "@App/app/cache_key";
 import { type ResourceBackup } from "@App/pkg/backup/struct";
-import { type VSCodeConnect } from "../offscreen/vscode-connect";
+import type { VSCodeConnectParam, VSCodeConnect } from "../offscreen/vscode-connect";
 import type { GMInfoEnv } from "../content/types";
 import { type SystemService } from "./system";
 import { type ScriptInfo } from "@App/pkg/utils/scriptInstall";
@@ -401,7 +401,7 @@ export class SystemClient extends Client {
     super(msgSender, "serviceWorker/system");
   }
 
-  connectVSCode(params: Parameters<VSCodeConnect["connect"]>[0]): ReturnType<VSCodeConnect["connect"]> {
+  connectVSCode(params: VSCodeConnectParam): ReturnType<VSCodeConnect["connect"]> {
     return this.do("connectVSCode", params);
   }
 
