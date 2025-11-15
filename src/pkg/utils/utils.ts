79c433481b9eaa7a33aa6b27dc6204fe87569c0d
王一之 <yz@ggnb.top>
2025-11-15 11:15:37 +0800

    删除不使用的方法

--- src/pkg/utils/utils.ts
+++ src/pkg/utils/utils.ts
@@ -369,11 +369,3 @@ export const stringMatching = (main: string, sub: string): boolean => {
     return false;
   }
 };
-
-export const urlSanitize = (url: string) => {
-  const u = new URL(url); // 利用 URL 处理 URL Encoding 问题。
-  // 例如 'https://日月.baidu.com/你好' => 'https://xn--wgv4y.baidu.com/%E4%BD%A0%E5%A5%BD'
-  // 为方便控制，只需要考虑 orign 和 pathname 的匹对
-  // https://user:passwd@httpbun.com/basic-auth/user/passwd -> https://httpbun.com/basic-auth/user/passwd
-  return `URL::${u.origin}${u.pathname}`;
-};
