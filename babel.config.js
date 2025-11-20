8ce5532b015e3c98fc7819bf6f50ad023a721046
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-11-20 22:00:41 +0900

    [MV2] Webpack, Babel 更新至 ES2015+

--- babel.config.js
+++ babel.config.js
@@ -3,38 +3,19 @@ module.exports = {
     [
       "@babel/preset-env",
       {
-        corejs: {
-          version: 3,
-        },
+        corejs: 3,
         useBuiltIns: "entry",
-        targets: "> 0.1%, last 2 versions, Firefox ESR, not dead",
+        targets: { esmodules: true }, // modern browsers (all ES2015+)
+        // targets: "> 0.1%, last 2 versions, Firefox ESR, not dead",
         bugfixes: true, // for tiny browser specific fixes
-        modules: false, // for webpack
-        // min versions: Chrome 62, Firefox 57, Safari 11.1, Edge 79 (ES2015+)
-        exclude: [
-          "@babel/plugin-transform-template-literals",
-          "@babel/plugin-transform-arrow-functions",
-          "@babel/plugin-transform-classes",
-          "@babel/plugin-transform-async-to-generator",
-          "@babel/plugin-transform-regenerator",
-          "@babel/plugin-transform-for-of",
-          "@babel/plugin-transform-block-scoping",
-          "@babel/plugin-transform-spread",
-          "@babel/plugin-transform-destructuring",
-          "@babel/plugin-transform-computed-properties",
-          "@babel/plugin-transform-shorthand-properties",
-          "@babel/plugin-transform-duplicate-keys",
-          "@babel/plugin-transform-literals",
-          "@babel/plugin-transform-member-expression-literals",
-          "@babel/plugin-transform-property-literals",
-        ],
+        // modules: false, // for webpack
       },
     ],
     "@babel/preset-react",
     "@babel/preset-typescript",
   ],
   plugins: [
-    "@babel/plugin-transform-runtime",
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    "@babel/plugin-transform-runtime", // helpers only, no corejs here
+    ["@babel/plugin-proposal-decorators", { legacy: true }], // only if you use them
   ],
 };
