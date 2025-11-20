753e5b9a5b9d8c6b601da0958fe831b93e9b30d6
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-11-20 21:13:14 +0900

    [MV2] Webpack, Babel 更新至 ES2015+

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,26 @@ module.exports = {
         },
         useBuiltIns: "entry",
         targets: "> 0.1%, last 2 versions, Firefox ESR, not dead",
+        bugfixes: true, // for tiny browser specific fixes
+        modules: false, // for webpack
+        // min versions: Chrome 62, Firefox 57, Safari 11.1, Edge 79 (ES2015+)
+        exclude: [
+          "@babel/plugin-transform-template-literals",
+          "@babel/plugin-transform-arrow-functions",
+          "@babel/plugin-transform-classes",
+          "@babel/plugin-transform-async-to-generator",
+          "@babel/plugin-transform-regenerator",
+          "@babel/plugin-transform-for-of",
+          "@babel/plugin-transform-block-scoping",
+          "@babel/plugin-transform-spread",
+          "@babel/plugin-transform-destructuring",
+          "@babel/plugin-transform-computed-properties",
+          "@babel/plugin-transform-shorthand-properties",
+          "@babel/plugin-transform-duplicate-keys",
+          "@babel/plugin-transform-literals",
+          "@babel/plugin-transform-member-expression-literals",
+          "@babel/plugin-transform-property-literals",
+        ],
       },
     ],
     "@babel/preset-react",
