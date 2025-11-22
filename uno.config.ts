bc373642e2cefc54333b19d2541e72be9d67787a
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-11-23 01:57:01 +0900

    presetUno -> presetWind3, 加 extractors 让 vscode 能配对

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig, presetUno } from "unocss";
+import { defineConfig, presetWind3 } from "unocss";
 
 export default defineConfig({
+  extractors: [
+    {
+      name: "prefix-tw",
+      extract({ code }) {
+        return Array.from(code.matchAll(/tw-[\w-:/]+/g)).map((i) => i[0]);
+      },
+    },
+  ],
   content: {
     filesystem: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   },
-  presets: [presetUno({ dark: "class", prefix: "tw-" })],
+  presets: [
+    presetWind3({
+      dark: "class",
+      prefix: "tw-",
+    }),
+  ],
 });
