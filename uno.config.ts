489be434284b6975e766c15769b004497e0061dd
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-11-23 14:21:56 +0900

    rspack/postcss/unocss 语法修改

--- uno.config.ts
+++ uno.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig, presetWind3 } from "unocss";
 
 export default defineConfig({
-  extractors: [
-    {
-      name: "prefix-tw",
-      extract({ code }) {
-        return Array.from(code.matchAll(/tw-[\w-:/]+/g)).map((i) => i[0]);
-      },
-    },
-  ],
   content: {
     filesystem: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   },
