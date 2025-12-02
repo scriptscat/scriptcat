fd88d015a9a02211b4b7cce924144c29079febe2
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-12-02 14:45:02 +0900

    🐛 修正`AppContext`错误引用`"monaco-editor"` (#983)

--- src/pages/store/AppContext.tsx
+++ src/pages/store/AppContext.tsx
@@ -1,6 +1,9 @@
 import React, { useState, createContext, type ReactNode, useEffect, useContext } from "react";
 import { messageQueue } from "./global";
-import { editor } from "monaco-editor";
+
+export const fnPlaceHolder = {
+  setEditorTheme: null,
+} as { setEditorTheme: ((theme: string) => void) | null };
 
 export type ThemeParam = { theme: "auto" | "light" | "dark" };
 export interface AppContextType {
@@ -46,12 +49,12 @@ const setAppColorTheme = (theme: "light" | "dark" | "auto") => {
     case "dark":
       document.documentElement.classList.add("dark");
       document.body.setAttribute("arco-theme", "dark");
-      editor.setTheme("vs-dark");
+      fnPlaceHolder.setEditorTheme?.("vs-dark");
       break;
     case "light":
       document.documentElement.classList.remove("dark");
       document.body.removeAttribute("arco-theme");
-      editor.setTheme("vs");
+      fnPlaceHolder.setEditorTheme?.("vs");
       break;
   }
 };
