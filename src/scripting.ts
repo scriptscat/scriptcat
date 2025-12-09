531ac109177b4275220305f85c53693ebf0261c8
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2025-12-09 13:01:04 +0900

    优化 scripting.ts 的代码设计

--- src/scripting.ts
+++ src/scripting.ts
@@ -3,41 +3,47 @@ import { createPageMessaging, pageDispatchCustomEvent } from "@Packages/message/
 import { uuidv5 } from "./pkg/utils/uuid";
 
 const scriptingMessaging = createPageMessaging("");
+const messageStack: any[] = [];
+
+// 在取得 scriptInjectMessageFlag 前，先堆叠一下，避免漏掉
+let dispatchDeliveryMessage = (message: any) => {
+  messageStack.push(message);
+};
+
+// ------------------------------
+chrome.storage.local.onChanged.addListener((changes) => {
+  if (changes["localStorage:scriptInjectMessageFlag"]?.newValue) {
+    dispatchDeliveryMessage({
+      tag: "localStorage:scriptInjectMessageFlag",
+      value: changes["localStorage:scriptInjectMessageFlag"]?.newValue,
+    });
+  }
+  if (changes["valueUpdateDelivery"]?.newValue) {
+    dispatchDeliveryMessage({
+      tag: "valueUpdateDelivery",
+      value: changes["valueUpdateDelivery"]?.newValue,
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, _sender) => {
+  if (!message) return;
+  const { action, data } = message;
+  dispatchDeliveryMessage({
+    tag: action,
+    value: data,
+  });
+});
 
 chrome.storage.local.get(["localStorage:scriptInjectMessageFlag"]).then((m) => {
   const MessageFlag = m["localStorage:scriptInjectMessageFlag"].value;
 
   const mainKey = uuidv5("scriptcat-listen-inject", MessageFlag);
 
-  const dispatchDeliveryMessage = (detail: any) => {
+  const dispatchDeliveryMessageAfterEtSet = (detail: any) => {
     if (!scriptingMessaging.et) throw new Error("scriptingMessaging is not ready or destroyed");
     pageDispatchCustomEvent(`evt_${scriptingMessaging.et}_deliveryMessage`, detail);
   };
-
-  // ------------------------------
-  chrome.storage.local.onChanged.addListener((changes) => {
-    if (changes["localStorage:scriptInjectMessageFlag"]?.newValue) {
-      dispatchDeliveryMessage({
-        tag: "localStorage:scriptInjectMessageFlag",
-        value: changes["localStorage:scriptInjectMessageFlag"]?.newValue,
-      });
-    }
-    if (changes["valueUpdateDelivery"]?.newValue) {
-      dispatchDeliveryMessage({
-        tag: "valueUpdateDelivery",
-        value: changes["valueUpdateDelivery"]?.newValue,
-      });
-    }
-  });
-
-  chrome.runtime.onMessage.addListener((message, _sender) => {
-    if (!message) return;
-    const { action, data } = message;
-    dispatchDeliveryMessage({
-      tag: action,
-      value: data,
-    });
-  });
 
   const injectFlag = randomMessageFlag();
   const injectFlagEvt = injectFlag;
@@ -48,6 +54,14 @@ chrome.storage.local.get(["localStorage:scriptInjectMessageFlag"]).then((m) => {
     (ev) => {
       if (ev instanceof CustomEvent && ev.detail?.[`emitterKeyFor${injectFlagEvt}`]) {
         scriptingMessaging.et = ev.detail[`emitterKeyFor${injectFlagEvt}`];
+        dispatchDeliveryMessage = dispatchDeliveryMessageAfterEtSet;
+        if (messageStack.length > 0) {
+          const messages = messageStack.slice();
+          messageStack.length = 0;
+          for (const message of messages) {
+            dispatchDeliveryMessage(message);
+          }
+        }
       }
     },
     { once: true }
