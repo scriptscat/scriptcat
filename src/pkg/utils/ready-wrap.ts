417d80aedafc4a343b08c0ff5f3d04d93f3ac93f
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-01-31 07:48:50 +0900

    ReadyWrap 釋放已使用 resolve, promise

--- src/pkg/utils/ready-wrap.ts
+++ src/pkg/utils/ready-wrap.ts
@@ -1,14 +1,16 @@
 export class ReadyWrap {
   public isReady: boolean = false;
-  private resolve: ((value: void | PromiseLike<void>) => void) | undefined;
-  private readonly promise: Promise<void> = new Promise<void>((resolve) => {
+  private resolve: ((value: void | PromiseLike<void>) => void) | null = null;
+  private promise: Promise<void> | null = new Promise<void>((resolve) => {
     this.resolve = resolve;
   });
   onReady(fn: () => any) {
-    this.isReady ? fn() : this.promise.then(fn);
+    this.isReady ? fn() : this.promise!.then(fn);
   }
   setReady() {
-    this.resolve!();
+    this.resolve?.();
     this.isReady = true;
+    this.resolve = null;
+    this.promise = null;
   }
 }
