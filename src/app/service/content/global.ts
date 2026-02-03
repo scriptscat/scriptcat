57ca95622dff8f8663f7ec1b871bc11e26d9516e
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-02-03 15:21:23 +0900

    Update global.ts

--- src/app/service/content/global.ts
+++ src/app/service/content/global.ts
@@ -1,5 +1,5 @@
 // 避免在全局页面环境中，内置处理函数被篡改或重写
-const unsupportedAPI = () => { throw new Error(); }
+const unsupportedAPI = () => { throw "unsupportedAPI"; }
 export const structuredClone_ = typeof structuredClone === "function" ? structuredClone : unsupportedAPI;
 export const jsonStringify_ = JSON.stringify.bind(JSON);
 export const jsonParse_ = JSON.parse.bind(JSON);
