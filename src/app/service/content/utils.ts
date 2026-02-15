23706090f9cb19c1b1c0195707a8dbd12be140d6
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-02-15 16:20:24 +0900

    让replace不识别$

--- src/app/service/content/utils.ts
+++ src/app/service/content/utils.ts
@@ -83,10 +83,10 @@ export function compileScriptCode(scriptRes: ScriptRunResource, scriptCode?: str
   });
 }
 
-const addTryCatch = (code: string) =>
-  `
+const addTryCatch = (code: string) => {
+  const templateText = `
       try {
-        {{functionBody}}
+        __FUNCTION_BODY__
       } catch (e) {
         if (e.message && e.stack) {
             console.error("ERROR: Execution of script '" + arguments[1] + "' failed! " + e.message);
@@ -95,11 +95,15 @@ const addTryCatch = (code: string) =>
             console.error(e);
         }
       }
-  `
+  `;
+  const text = templateText
     .trim()
     .replace(/[\r\n]/g, "")
-    .replace(/\s+/g, " ")
-    .replace("{{functionBody}}", () => code);
+    .replace(/\s+/g, " ");
+  // 用 function replacer 让replace不识别$
+  const output = text.replace(/\s*__FUNCTION_BODY__\s*/, () => "\n" + code + "\n");
+  return output;
+};
 
 export function compileScriptCodeByResource(resource: CompileScriptCodeResource): string {
   const requireCode = resource.require.map((r) => r.content).join("\n;");
