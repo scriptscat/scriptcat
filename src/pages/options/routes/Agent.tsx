1639aa65de7b41e07f1ae38622b77a6cc399ac67
王一之 <yz@ggnb.top>
2026-03-12 21:25:22 +0800

    🧹 chore: 移除未使用的 ComingSoon 组件

--- src/pages/options/routes/Agent.tsx
+++ src/pages/options/routes/Agent.tsx
@@ -1,22 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { Card, Space, Typography } from "@arco-design/web-react";
 import AgentProvider from "./AgentProvider";
 import AgentChat from "./AgentChat";
 import AgentMcp from "./AgentMcp";
 import AgentOPFS from "./AgentOPFS";
 import AgentSkills from "./AgentSkills";
-
-function ComingSoon() {
-  const { t } = useTranslation();
-  return (
-    <Space className="tw-w-full tw-h-full tw-overflow-auto tw-relative" direction="vertical">
-      <Card bordered={false}>
-        <Typography.Text type="secondary">{t("agent_coming_soon")}</Typography.Text>
-      </Card>
-    </Space>
-  );
-}
 
 function Agent() {
   return (
