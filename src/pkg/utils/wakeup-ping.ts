adebb9bcaf62453cecb441586937e4451847f272
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-04-25 09:52:31 +0900

    改为 wakeupPingCommand ：只在需要时 ping

--- src/pkg/utils/wakeup-ping.ts
+++ src/pkg/utils/wakeup-ping.ts
@@ -1,6 +1,13 @@
 const PING_INTERVAL_MS_1 = 13_225;
 const PING_INTERVAL_MS_2 = 17_765;
 
+export const WakeUpCommand = {
+  START: 0x100,
+  STOP: 0x200,
+} as const;
+
+export type WakeUpCommand = ValueOf<typeof WakeUpCommand>;
+
 /**
  * scheduler 用于后台排程：Chrome 94+, Firefox 142+
  * @link https://developer.mozilla.org/en-US/docs/Web/API/Scheduler/postTask
@@ -10,17 +17,22 @@ const nativeScheduler =
   typeof scheduler !== "undefined" && typeof scheduler?.postTask === "function" && scheduler;
 
 // 高效的 BroadcastChannel 通讯：service worker 和 offscreen 共用同一通道
-const channel = new BroadcastChannel("custom-ping");
+const channel = new BroadcastChannel("custom-ping"); // offscreen -> sw
+const channelCommand = new BroadcastChannel("custom-ping-command"); // sw -> offscreen
+let startCounter = 0;
 
-export const startRepetitivePing = () => {
+let incrementCounter = () => {};
+
+// initializeWakeupPing only execute once in offscreen
+export const initializeWakeupPing = () => {
   if (typeof frameElement === "object" && typeof document === "object" && document) {
     let counter = 0;
     let isMutationPending = false;
 
     const pingNode = document.createComment("0");
 
-    const incrementCounter = () => {
-      if (!isMutationPending) {
+    incrementCounter = () => {
+      if (startCounter >= 1 && !isMutationPending) {
         isMutationPending = true;
         counter = counter > 8 ? 1 : counter + 1;
         pingNode.data = `${counter}`;
@@ -44,10 +56,22 @@ export const startRepetitivePing = () => {
       }
     });
     mutationObserver.observe(pingNode, { characterData: true });
-    incrementCounter();
+    // incrementCounter();
+
+    channelCommand.onmessage = (e) => {
+      if (e.data === WakeUpCommand.START) {
+        startCounter++;
+        if (startCounter === 1) {
+          incrementCounter();
+        }
+      } else if (e.data === WakeUpCommand.STOP) {
+        if (startCounter > 0) startCounter--;
+      }
+    };
   }
 };
 
+// initializeWakeupPing only execute once in service worker
 export const listenWakeupPing = (onWakeupPing: (...args: any) => any) => {
   chrome.storage.session.onChanged.addListener((obj) => {
     // 消耗 persistentWakeup
@@ -60,4 +84,9 @@ export const listenWakeupPing = (onWakeupPing: (...args: any) => any) => {
     // 触发 chrome storage onChanged 使 service worker 保持活跃
     chrome.storage.session.set({ persistentWakeup: `${e.timeStamp}` });
   };
+};
+
+// wakeupPingCommand only execute in service worker
+export const wakeupPingCommand = (command: WakeUpCommand) => {
+  channelCommand.postMessage(command);
 };
