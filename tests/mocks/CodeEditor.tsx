1e80e2717f0c02c2967a9b873c5f76a1544aaace
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-06-18 20:00:19 +0900

    vitest页面测试加速

--- tests/mocks/CodeEditor.tsx
+++ tests/mocks/CodeEditor.tsx
@@ -1,11 +1,3 @@
-export default function MockCodeEditor({
-  id,
-  code,
-  diffCode,
-}: {
-  id: string;
-  code?: string;
-  diffCode?: string;
-}) {
+export default function MockCodeEditor({ id, code, diffCode }: { id: string; code?: string; diffCode?: string }) {
   return <div data-testid="code-body" data-id={id} data-code={code} data-diff={diffCode ?? ""} />;
 }
