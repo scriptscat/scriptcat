d39174e0ef37fa59eaffaee413f0ad5b04de597b
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-09-19 07:07:06 +0900

    `GM_webRequest` -> `GM_testingAPI`

--- src/pages/install/useInstallData.test.ts
+++ src/pages/install/useInstallData.test.ts
@@ -122,12 +122,12 @@ describe("assembleInstallView 组装安装视图", () => {
       name: ["示例脚本"],
       version: ["2.3.1"],
       "exclude-match": ["*://a.com/*"],
-      grant: ["GM_webRequest"],
+      grant: ["GM_testingAPI"],
     };
     const code = `// ==UserScript==
 // @name 示例脚本
 // @exclude-match *://a.com/*
-// @grant GM_webRequest
+// @grant GM_testingAPI
 // ==/UserScript==`;
     const view = assembleInstallView({
       isUpdate: false,
@@ -136,7 +136,7 @@ describe("assembleInstallView 组装安装视图", () => {
       code,
       oldVersion: null,
     });
-    expect(view.compat.grants).toEqual(new Map([["GM_webRequest", 4]]));
+    expect(view.compat.grants).toEqual(new Map([["GM_testingAPI", 4]]));
     expect(view.compat.tags).toEqual([{ tag: "exclude-match", line: 3 }]);
     expect(view.compat.scriptcatOnlyTags).toEqual([]);
   });
