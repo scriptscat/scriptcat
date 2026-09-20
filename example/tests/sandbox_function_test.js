277d449f2b307393392af3747089aa3746ffa346
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-09-20 17:38:22 +0900

    🐛 update SCTest example pins

--- example/tests/sandbox_function_test.js
+++ example/tests/sandbox_function_test.js
@@ -19,7 +19,7 @@
 // @grant        window.close
 // @grant        window.focus
 // @grant        unsafeWindow
-// @require      https://cdn.jsdelivr.net/gh/scriptscat/scriptcat@b8c6d0839c75ee5e4e4276dd10e201011c445df8/example/tests/lib/sctest.js
+// @require      https://cdn.jsdelivr.net/gh/scriptscat/scriptcat@36ab4ce5ff23c820a32cd13ac5a04d8834ab4d82/example/tests/lib/sctest.js
 // @run-at       document-end
 // ==/UserScript==
 
