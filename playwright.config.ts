faba7d843f5a067c7f1a2d34194a90ee8c787ff7
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2026-09-22 17:38:58 +0900

    🔧 限制本地测试为单 worker

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 2 : 4,
+  workers: process.env.CI ? 2 : 1,
   reporter: process.env.CI ? [["html", { open: "never" }], ["list"]] : "list",
   outputDir: "test-results",
   use: {
